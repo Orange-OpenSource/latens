9ed961a44d9648fc88f48ecca86092cd74c32e71
Christophe Maldivi <christophe.maldivi@orange.com>
2014-08-08 16:36:09 +0200

    start adding roboelectric

---
+++ src/test/java/com/orange/latens/core/stats/LatensPointTest.java
@@ -0,0 +1,19 @@
+package com.orange.latens.core.stats;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class LatensPointTest {
+
+  @Test
+  public void shouldTest() {
+    Assert.assertTrue(true);
+  }
+
+}
