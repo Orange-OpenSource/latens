d70e84319b8ed624c7f1c8dca68939091a171aa6
Christophe Maldivi <christophe.maldivi@orange.com>
2014-08-30 21:48:58 +0200

    unit tests

--- src/test/java/com/orange/latens/core/stats/LatensStatsRefreshAnalysisDurationTest.java
+++ src/test/java/com/orange/latens/core/stats/LatensStatsRefreshAnalysisDurationTest.java
@@ -1,5 +1,9 @@
 package com.orange.latens.core.stats;
 
+import com.orange.latens.TestTools;
+import com.orange.latens.preferences.Preferences;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -13,12 +17,15 @@ public class LatensStatsRefreshAnalysisDurationTest {
   @Test
   public void shouldRefreshAnalysisDuration() throws IllegalAccessException {
     // given
-    int analysisDuration = 1235;
+    long analysisDuration = 1235;
     LatensStats latensStats = Mockito.mock(LatensStats.class);
     Mockito.doCallRealMethod().when(latensStats).refreshAnalysisDuration();
+    Preferences preferences = Mockito.mock(Preferences.class);
+    Mockito.when(preferences.getAnalysisDurationMs(null)).thenReturn(analysisDuration);
+    TestTools.setObj("preferences", LatensStats.class, latensStats, preferences);
     // do
-    //latensStats.refreshAnalysisDuration();
+    latensStats.refreshAnalysisDuration();
     // then
-    //Assert.assertEquals(analysisDuration, TestTools.getObj("analysisDuration", LatensStats.class, latensStats));
+    Assert.assertEquals(analysisDuration, TestTools.getObj("analysisDuration", LatensStats.class, latensStats));
   }
 }
